f90ac4befd3ffdb7cf9d75cd8325cdaf5d42ea66
Roman <RLpostbox@yandex.ru>
2018-06-12 21:57:20 +0300

    work rest api

--- test_work_project/messages_app/serializers.py
+++ test_work_project/messages_app/serializers.py
@@ -1,48 +1,9 @@
-from messages_app.models import Message
-from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
-class MessageSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    text_message = serializers.CharField(default='no text')
-    user = serializers.CharField(read_only=True)
-    data_create = serializers.DateField(read_only=True)
-    date_last_chenges = serializers.DateField(read_only=True)
-
-    def create(self, validated_data):
-        return Message.objects.create(**validated_data)
+from .models import Message
 
 
-    def update(self, instance, validated_data):
-        instance.text_message = validated_date.get('text_message', instance.text_message)
-        return instance
-
-
-    def getMessagesOnPk(self, primKey):
-        return Message.objects.get(pk=primKey)
-
-
-    def deleteMessagesOnPk(self, primKey):
-        return Message.objects.filter(pk=primKey).delete()
-
-
-    def getAllMessages(self):
-        return Message.objects.all()
-
-"""
-class MessageSerializer(serializers.HyperlinkedModelSerializer):
+class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
-        fields = ('user', 'data_create', 'data_last_cheges', 'text_message')
-"""
-
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'groups')
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ('url', 'name')
+        fields = "__all__"
--- test_work_project/test_work_project/urls.py
+++ test_work_project/test_work_project/urls.py
@@ -1,61 +1,10 @@
-"""test_work_project URL Configuration
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.conf.urls import url, include
-from messages_app import models, serializers, views
-from messages_app.models import Message
-from django.urls import path
-from django.contrib.auth import views as  auth_views
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
-from messages_app.serializers import MessageSerializer, UserSerializer, GroupSerializer
-from messages_app.views import MessageViewSet, UserViewSet, GroupViewSet
-from messages_app import urls
+from django.contrib import admin
 
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ('url', 'username', 'email', 'is_staff')
-
-# ViewSets define the view behavior.
-#class UserViewSet(viewsets.ModelViewSet):
-#    queryset = User.objects.all()
-#    serializer_class = UserSerializers
-
-
-#class MessageViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows messages to be viewed
-    """
-#    queryset = Message.objects.all()
-#    serializer_class = MessageSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-router.register(r'messages', MessageViewSet)
+from messages_app import urls as messages_urls
 
 urlpatterns = [
-   # path('admin/', admin.site.urls),
-   # url(r'^ login / $', auth_views.login),
-   # url(r'^$', views.empty_page, name='empty_page'),
-	url(r'^api/', include(urls)),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    url(r'^', include(router.urls)),
-    url(r'^', include('messages_app.urls')),
+    url(r'^api/', include(messages_urls)),
+    url(r'^admin/', admin.site.urls),
 ]
