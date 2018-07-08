855f04f198c8de778449903f3330c8b611dab298
Roman <RLpostbox@yandex.ru>
2018-07-09 01:01:22 +0300

    The first steps to api-message history

--- test_work_project/messages_app/urls.py
+++ test_work_project/messages_app/urls.py
@@ -1,9 +1,13 @@
 from rest_framework.routers import SimpleRouter
 
 from .views import SubscriberViewSet
+from simple_history.models import HistoricalRecords
 
 router = SimpleRouter()
 router.register("messages", SubscriberViewSet)
+router.register("messages/([0-9]+)/history", SubscriberViewSet) 
+#router.register("messages/([0-9]+)/history", SubscriberViewSet.id_history()) 
+#router.register("messages/([0-9]+)/history", SubscriberViewSet.id_history(1)) 
 
 urlpatterns = router.urls
 
--- test_work_project/messages_app/views.py
+++ test_work_project/messages_app/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
 
 from .serializers import MessageSerializer
 from .models import Message
@@ -11,24 +12,10 @@ class SubscriberViewSet(ModelViewSet):
     queryset = Message.objects.all()
     #permission_classes = (IsAuthenticated,)
 
-
 """
-from django.contrib.auth import authenticate
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from rest_framework.status import HTTP_401_UNAUTHORIZED
-from rest_framework.authtoken.models import Token
-
-
-@api_view(["POST"])
-def login(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-
-    user = authenticate(username=username, password=password)
-    if not user:
-        return Response({"error": "Login failed"}, status=HTTP_401_UNAUTHORIZED)
-
-    token, _ = Token.objects.get_or_create(user=user)
-    return Response({"token": token.key})
+    @action(methods=['get'], detail=True,
+            url_path='history', url_name='history')
+    def id_history():
+        pass
+        #return SubscriberViewSet.queryset[1]
 """
